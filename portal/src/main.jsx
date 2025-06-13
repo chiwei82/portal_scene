25fb111cb337152b7ee5e4bbd0049490638411bf
CHIWHI, FNEG <55752357+chiwei82@users.noreply.github.com>
2025-06-13 15:41:52 +0800

    add Loader

--- portal/src/main.jsx
+++ portal/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
+import { Loader } from '@react-three/drei'
 import './index.css'
 import App from './App.jsx'
 import Particle from './particle'
@@ -19,5 +20,6 @@ createRoot(document.getElementById('root')).render(
       <App />
       <Particle />
     </Canvas>
+    <Loader />
   </StrictMode>,
 )
